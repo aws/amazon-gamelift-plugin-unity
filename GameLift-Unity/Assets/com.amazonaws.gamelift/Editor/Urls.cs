e560393ecf8e2ee1d371136e1943efd152afa086
James Miao <miaox@amazon.com>
2021-09-15 22:08:18 -0700

    Update help URLs

--- GameLift-Unity/Assets/com.amazonaws.gamelift/Editor/Urls.cs
+++ GameLift-Unity/Assets/com.amazonaws.gamelift/Editor/Urls.cs
@@ -10,10 +10,9 @@ namespace AmazonGameLift.Editor
         public const string AwsGameLiftLocal = "https://gamelift-release.s3-us-west-2.amazonaws.com/GameLift_06_03_2021.zip";
         public const string AwsGameTechForums = "https://forums.awsgametech.com/";
         public const string AwsHelpCredentials = "https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html";
-        public const string AwsHelpDeployment = "https://aws.amazon.com/";
-        public const string AwsHelpGameLiftLocal = "https://aws.amazon.com/";
-        public const string AwsHelpGameLiftEndpointsQuotas = "https://docs.aws.amazon.com/general/latest/gr/gamelift.html";
-        public const string AwsHelpGameLiftUnity = "https://docs.aws.amazon.com/gamelift/latest/developerguide/integration-engines-unity-using.html";
+        public const string AwsHelpDeployment = "https://docs.aws.amazon.com/gamelift/latest/developerguide/integrate-unitypi.html";
+        public const string AwsHelpGameLiftLocal = "https://docs.aws.amazon.com/gamelift/latest/developerguide/integration-testing-local.html";
+        public const string AwsHelpGameLiftUnity = "https://docs.aws.amazon.com/gamelift/latest/developerguide/integrate-unitypi.html";
         public const string AwsS3Console = "https://s3.console.aws.amazon.com/s3/home";
         public const string AwsS3BucketTemplate = "https://s3.console.aws.amazon.com/s3/buckets/{0}?region={1}";
         public const string AwsSecurity = "https://aws.amazon.com/security/vulnerability-reporting/";
