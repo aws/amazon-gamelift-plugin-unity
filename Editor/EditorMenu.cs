8bff9fccae1ed5f0b5e68744fac5aa0a29007657
James Miao <jmiaoxh@gmail.com>
2021-11-16 16:56:02 -0800

    Open project browser when pinging SDK DLLs (#49)
    * Open Project Browser when ping SDK DLL; gitignore *.meta files
    
    * Only ignore iml and iml.meta files
    
    Co-authored-by: James Miao <miaox@amazon.com>

--- Editor/EditorMenu.cs
+++ Editor/EditorMenu.cs
@@ -77,10 +77,22 @@ namespace AmazonGameLift.Editor
         {
             string filePackagePath = $"Packages/{Paths.PackageName}/{Paths.ServerSdkDllInPackage}";
             UnityEngine.Object sdk = AssetDatabase.LoadMainAssetAtPath(filePackagePath);
+            Type projectBrowser = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.ProjectBrowser");
 
-            if (sdk)
+            if (projectBrowser != null) {
+                // Show Project Browser window
+                EditorWindow.GetWindow(projectBrowser).Show();
+            } else {
+                Debug.LogError($"Cannot open the Unity Project Browser window.");
+                return;
+            }
+
+            if (sdk != null)
             {
+                // Show SDK DLL in Unity Inspector
                 Selection.activeObject = sdk;
+
+                // Highlight SDK DLL in Unity Project Browser
                 EditorGUIUtility.PingObject(sdk);
             } else {
                 Debug.LogError($"Cannot find GameLift SDK DLL in asset path: {filePackagePath}. "
