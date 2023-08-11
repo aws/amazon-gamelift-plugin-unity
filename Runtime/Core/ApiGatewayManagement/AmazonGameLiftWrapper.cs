2293dc06101c51f00603943a8fa0ae3d7e9c7c32
Carl <46359733+ChefCarlos@users.noreply.github.com>
2023-08-11 19:16:33 +0100

    ASG3: Renamed AmazonGameLiftWrapper; Added Server API Calls; (#88)
    * ASG3: Renamed AmazonGameLiftWrapper; Added Server API Calls;
    
    * ASG3: Removed overloaded methods to be used in the UI;
    
    * ASG3: Removed Unused Variables and Declarations

---
+++ Runtime/Core/ApiGatewayManagement/AmazonGameLiftWrapper.cs
@@ -0,0 +1,80 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.GameLift;
+using Amazon.GameLift.Model;
+
+namespace AmazonGameLiftPlugin.Core.ApiGatewayManagement
+{
+    public class AmazonGameLiftWrapper : IAmazonGameLiftClientWrapper
+    {
+        private readonly IAmazonGameLift _amazonGameLiftClient;
+
+        internal AmazonGameLiftWrapper(IAmazonGameLift amazonGameLiftClient)
+        {
+            _amazonGameLiftClient = amazonGameLiftClient;
+        }
+
+        /// <summary>
+        /// Client region is code dedicated to Amazon GameLift SDK calls made by the game client. 
+        /// </summary>
+        #region Client
+        
+        public async Task<CreateGameSessionResponse> CreateGameSessionAsync(
+                CreateGameSessionRequest request,
+                CancellationToken cancellationToken = default
+            )
+        {
+            return await _amazonGameLiftClient.CreateGameSessionAsync(request, cancellationToken);
+        }
+
+        public async Task<CreatePlayerSessionResponse> CreatePlayerSession(CreatePlayerSessionRequest request)
+        {
+            return await _amazonGameLiftClient.CreatePlayerSessionAsync(request);
+        }
+
+        public async Task<SearchGameSessionsResponse> SearchGameSessions(SearchGameSessionsRequest request)
+        {
+            return await _amazonGameLiftClient.SearchGameSessionsAsync(request);
+        }
+
+        public async Task<DescribeGameSessionsResponse> DescribeGameSessions(DescribeGameSessionsRequest request)
+        {
+            return await _amazonGameLiftClient.DescribeGameSessionsAsync(request);
+        }
+        #endregion
+        
+        /// <summary>
+        /// Server region is code dedicated to Amazon GameLift SDK and AWS SDK calls made by the game server. All of these calls will be done via UI Elements or on Startup. 
+        /// </summary>
+        #region Server
+
+        public async Task<ListLocationsResponse> ListLocations(ListLocationsRequest request)
+        {
+            return await _amazonGameLiftClient.ListLocationsAsync(request);
+        }
+
+        public async Task<CreateLocationResponse> CreateLocation(CreateLocationRequest request)
+        {
+            return await _amazonGameLiftClient.CreateLocationAsync(request);
+        }
+
+        public async Task<RegisterComputeResponse> RegisterCompute(RegisterComputeRequest request)
+        {
+            return await _amazonGameLiftClient.RegisterComputeAsync(request);
+        }
+
+        public async Task<GetComputeAuthTokenResponse> GetComputeAuthToken(GetComputeAuthTokenRequest request)
+        {
+            return await _amazonGameLiftClient.GetComputeAuthTokenAsync(request);
+        }
+
+        public Task<CreateFleetResponse> CreateFleet(CreateFleetRequest request)
+        {
+            return _amazonGameLiftClient.CreateFleetAsync(request);
+        }
+        #endregion
+    }
+}
