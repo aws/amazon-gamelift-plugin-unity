a177576c5c58ef42a977b4c50b2cbb2a51406371
Kyle Young <kyle.young@codewizards.co.uk>
2023-08-28 16:11:24 +0100

    Added ElementLocalizer to manage common localization code

---
+++ Editor/Resources/EditorWindow/ElementLocalizer.cs
@@ -0,0 +1,25 @@
+﻿using AmazonGameLift.Editor;
+using UnityEngine.UIElements;
+
+namespace Editor.Resources.EditorWindow
+{
+    public class ElementLocalizer
+    {
+        private readonly VisualElement _root;
+        private readonly TextProvider _textProvider = TextProviderFactory.Create();
+
+        public ElementLocalizer(VisualElement root)
+        {
+            _root = root;
+        }
+
+        public void SetElementText(string elementName, string textKey)
+        {
+            var element = _root.Q<TextElement>(elementName);
+            if (element != default)
+            {
+                element.text = _textProvider.Get(textKey);
+            }
+        }
+    }
+}
