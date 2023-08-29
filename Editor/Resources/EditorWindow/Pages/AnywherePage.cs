7673e0c6289d3e9dc9b11c185c573557ae08ff36
Kyle Young <kyle.young@codewizards.co.uk>
2023-08-29 16:51:49 +0100

    Added visual for anywhere page

--- Editor/Resources/EditorWindow/Pages/AnywherePage.cs
+++ Editor/Resources/EditorWindow/Pages/AnywherePage.cs
@@ -8,6 +8,13 @@ namespace Editor.Resources.EditorWindow.Pages
     public class AnywherePage
     {
         private readonly VisualElement _container;
+        private readonly VisualElement _fleetNameInput;
+        private readonly VisualElement _fleetNameDropdownContainer;
+        private readonly VisualElement _fleetID;
+        private readonly VisualElement _fleetStatus;
+
+        private FleetStatus _fleetState;
+        private readonly DropdownField _fleetNameDropdown;
 
         public AnywherePage(VisualElement container)
         {
@@ -17,6 +24,39 @@ namespace Editor.Resources.EditorWindow.Pages
 
             container.Add(uxml);
             ApplyText();
+
+            _fleetState = FleetStatus.Creating;
+
+            _fleetNameInput = container.Q("LabelAnywhereConnectNoFleet");
+            _fleetNameDropdownContainer = container.Q("LabelAnywhereConnectFleets");
+            _fleetNameDropdown = container.Q<DropdownField>("LabelAnywhereConnectFleets");
+            _fleetID = container.Q("AnywhereFleetID");
+            _fleetStatus = container.Q("AnywhereFleetStatus");
+            
+            UpdateGUI();
+        }
+
+        private void UpdateGUI()
+        {
+            switch (_fleetState)
+            {
+                case FleetStatus.Creating:
+                    _fleetNameInput.RemoveFromClassList("foldout--hidden");
+                    _fleetNameDropdownContainer.AddToClassList("foldout--hidden");
+                    break;
+                case FleetStatus.Selecting:
+                    _fleetNameInput.AddToClassList("foldout--hidden");
+                    _fleetNameDropdownContainer.RemoveFromClassList("foldout--hidden");
+                    _fleetID.AddToClassList("foldout--hidden");
+                    _fleetStatus.AddToClassList("foldout--hidden");
+                    break;
+                case FleetStatus.Selected:
+                    _fleetNameInput.AddToClassList("foldout--hidden");
+                    _fleetNameDropdownContainer.RemoveFromClassList("foldout--hidden");
+                    _fleetID.RemoveFromClassList("foldout--hidden");
+                    _fleetStatus.RemoveFromClassList("foldout--hidden");
+                    break;
+            }
         }
 
         private void ApplyText()
@@ -24,5 +64,12 @@ namespace Editor.Resources.EditorWindow.Pages
             var l = new ElementLocalizer(_container);
             // l.SetElementText("LabelAnywhereIntegrateTitle", "");
         }
+    }
+
+    public enum FleetStatus
+    {
+        Creating,
+        Selecting,
+        Selected
     }
 }
