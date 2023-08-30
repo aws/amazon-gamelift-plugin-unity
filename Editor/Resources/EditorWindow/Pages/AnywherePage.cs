35c3729c3a234bb0feb6c5c50de1c18e0ebdf5cd
Kyle Young <kyle.young@codewizards.co.uk>
2023-08-30 11:04:02 +0100

    Seperated connect to fleet section to new class

--- Editor/Resources/EditorWindow/Pages/AnywherePage.cs
+++ Editor/Resources/EditorWindow/Pages/AnywherePage.cs
@@ -8,13 +8,9 @@ namespace Editor.Resources.EditorWindow.Pages
     public class AnywherePage
     {
         private readonly VisualElement _container;
-        private readonly VisualElement _fleetNameInput;
-        private readonly VisualElement _fleetNameDropdownContainer;
-        private readonly VisualElement _fleetID;
-        private readonly VisualElement _fleetStatus;
 
-        private FleetStatus _fleetState;
-        private readonly DropdownField _fleetNameDropdown;
+
+        private DropdownField _fleetNameDropdown;
 
         public AnywherePage(VisualElement container)
         {
@@ -25,39 +21,9 @@ namespace Editor.Resources.EditorWindow.Pages
             container.Add(uxml);
             ApplyText();
 
-            _fleetState = FleetStatus.Creating;
-
-            _fleetNameInput = container.Q("LabelAnywhereConnectNoFleet");
-            _fleetNameDropdownContainer = container.Q("LabelAnywhereConnectFleets");
-            _fleetNameDropdown = container.Q<DropdownField>("LabelAnywhereConnectFleets");
-            _fleetID = container.Q("AnywhereFleetID");
-            _fleetStatus = container.Q("AnywhereFleetStatus");
-            
-            UpdateGUI();
+            var fleetInput = new ConnectToFleetInput(container, ConnectToFleetInput.FleetStatus.CreatingInitial);
         }
 
-        private void UpdateGUI()
-        {
-            switch (_fleetState)
-            {
-                case FleetStatus.Creating:
-                    _fleetNameInput.RemoveFromClassList("foldout--hidden");
-                    _fleetNameDropdownContainer.AddToClassList("foldout--hidden");
-                    break;
-                case FleetStatus.Selecting:
-                    _fleetNameInput.AddToClassList("foldout--hidden");
-                    _fleetNameDropdownContainer.RemoveFromClassList("foldout--hidden");
-                    _fleetID.AddToClassList("foldout--hidden");
-                    _fleetStatus.AddToClassList("foldout--hidden");
-                    break;
-                case FleetStatus.Selected:
-                    _fleetNameInput.AddToClassList("foldout--hidden");
-                    _fleetNameDropdownContainer.RemoveFromClassList("foldout--hidden");
-                    _fleetID.RemoveFromClassList("foldout--hidden");
-                    _fleetStatus.RemoveFromClassList("foldout--hidden");
-                    break;
-            }
-        }
 
         private void ApplyText()
         {
@@ -66,10 +32,4 @@ namespace Editor.Resources.EditorWindow.Pages
         }
     }
 
-    public enum FleetStatus
-    {
-        Creating,
-        Selecting,
-        Selected
-    }
 }
---
+++ Editor/Resources/EditorWindow/Pages/ConnectToFleetInput.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine.UIElements;
+
+namespace Editor.Resources.EditorWindow.Pages
+{
+    public class ConnectToFleetInput
+    {
+        private const string InactiveClassName = "foldout--hidden";
+        
+        private readonly VisualElement _fleetNameInput;
+        private readonly VisualElement _fleetNameDropdownContainer;
+        private readonly VisualElement _fleetID;
+        private readonly VisualElement _fleetStatus;
+
+        private FleetStatus _fleetState;
+        private readonly Button _cancelButton;
+
+        public ConnectToFleetInput(VisualElement container, FleetStatus initialState)
+        {
+            _fleetState = initialState;
+
+            _fleetNameInput = container.Q("LabelAnywhereConnectNoFleet");
+            _fleetNameDropdownContainer = container.Q("LabelAnywhereConnectFleets");
+            _fleetID = container.Q("AnywhereFleetID");
+            _fleetStatus = container.Q("AnywhereFleetStatus");
+
+            container.Q<Button>("ButtonAnywhereConnectButton").RegisterCallback<ClickEvent>(_ =>
+            {
+                if (_fleetState is FleetStatus.CreatingInitial or FleetStatus.Creating)
+                {
+                    // TODO: Add functionality
+                    _fleetState = FleetStatus.Selected;
+                }
+                UpdateGUI();
+            });
+
+            container.Q<Button>("AnywhereFleetCreateNewButton").RegisterCallback<ClickEvent>(_ =>
+            {
+                if (_fleetState is FleetStatus.Selected or FleetStatus.Selecting)
+                {
+                    // TODO: Add functionality
+                    _fleetState = FleetStatus.Creating;
+                }
+                UpdateGUI();
+
+            });
+
+            _cancelButton = container.Q<Button>("AnywhereFleetCancelButton");
+            _cancelButton.RegisterCallback<ClickEvent>(_ =>
+            {
+                if (_fleetState is FleetStatus.Creating)
+                {
+                    // TODO: Add functionality
+                    _fleetState = FleetStatus.Selected;
+                }
+                UpdateGUI();
+            });
+
+            UpdateGUI();
+        }
+
+        private static void Hide(VisualElement element) => element.AddToClassList(InactiveClassName);
+        private static void Show(VisualElement element) => element.RemoveFromClassList(InactiveClassName);
+
+        private void UpdateGUI()
+        {
+            switch (_fleetState)
+            {
+                case FleetStatus.Creating:
+                    Show(_fleetNameInput);
+                    Show(_cancelButton);
+                    Hide(_fleetNameDropdownContainer); ;
+                    break;
+                case FleetStatus.CreatingInitial:
+                    Show(_fleetNameInput);
+                    Hide(_cancelButton);
+                    Hide(_fleetNameDropdownContainer);
+                    break;
+                case FleetStatus.Selecting:
+                    Show(_fleetNameDropdownContainer);
+                    Hide(_fleetNameInput);
+                    Hide(_fleetID);
+                    Hide(_fleetStatus);
+                    break;
+                case FleetStatus.Selected:
+                    Show(_fleetNameDropdownContainer);
+                    Show(_fleetID);
+                    Show(_fleetStatus);
+                    Hide(_fleetNameInput);
+                    break;
+            }
+        }
+
+        public enum FleetStatus
+        {
+            CreatingInitial,
+            Creating,
+            Selecting,
+            Selected
+        }
+    }
+}
