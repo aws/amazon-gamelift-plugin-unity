fa8f42fb1230556933876891a95de5f84a99901b
Kyle-CW <109524503+Kyle-CW@users.noreply.github.com>
2023-09-15 21:43:29 +0100

    ASG6 Removed old editor menu (#98)
    * Created new plugin window
    Created new file menu
    
    * Added use of TextProvider to plugin
    
    * Updated Help tab text
    
    * Removed incorrect code
    
    * Removed old plugin menu
    
    * Created new class for new plugin menu
    
    * ASG6: Deleted More Old Plugin Code;
    
    * Use correct GameLiftPlugin file
    
    * Made required changes
    
    * Added icon
    
    * Moved EditorMenu.cs
    
    * Removed unsued Settings keys
    
    * Removed unused method
    
    ---------
    
    Co-authored-by: ChefCarlos <carlind95@gmail.com>

--- Editor/CoreAPI/SettingsKeys.cs
+++ Editor/CoreAPI/SettingsKeys.cs
@@ -8,13 +8,9 @@ namespace AmazonGameLift.Editor
         public const string CurrentBucketName = "CurrentBucketName";
         public const string CurrentProfileName = "CurrentProfileName";
         public const string CurrentRegion = "CurrentRegion";
-        public const string GameLiftLocalPath = "GameLiftLocalPath";
-        public const string GameLiftLocalPort = "GameLiftLocalPort";
-        public const string LocalServerPath = "LocalServerPath";
         public const string DeploymentScenarioIndex = "DeploymentScenarioIndex";
         public const string DeploymentGameName = "DeploymentGameName";
         public const string DeploymentBuildFilePath = "DeploymentBuildFilePath";
         public const string DeploymentBuildFolderPath = "DeploymentBuildFolderPath";
-        public const string WasSettingsWindowShown = "WasSettingsWindowShown";
     }
 }
---
+++ Editor/Window/EditorMenu.cs
@@ -0,0 +1,78 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using AmazonGameLift.Editor;
+using Editor.Window;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Resources.EditorWindow
+{
+    internal class EditorMenu
+    {
+        private static readonly Type _inspectorType = Type.GetType("UnityEditor.GameView,UnityEditor.dll");
+        private static readonly string _filePackagePath = $"Packages/{Paths.PackageName}/{Paths.SampleGameInPackage}";
+
+        private static GameLiftPlugin GetPluginWindow()
+        {
+            var window = UnityEditor.EditorWindow.GetWindow<GameLiftPlugin>(_inspectorType);
+            window.titleContent = new GUIContent("Amazon GameLift", window.Icon);
+            return window;
+        }
+
+        [MenuItem("Amazon GameLift/Show Amazon GameLift Window", priority = 0)]
+        public static void ShowWindow()
+        {
+            GetPluginWindow();
+        }
+
+        [MenuItem("Amazon GameLift/Bring Panel to Front", priority = 1)]
+        public static void FocusPanel()
+        {
+            ShowWindow();
+        }
+
+        [MenuItem("Amazon GameLift/Set AWS Account Profiles", priority = 100)]
+        public static void OpenAccountProfilesTab()
+        {
+            GetPluginWindow().OpenTab(GameLiftPlugin.Pages.Credentials);
+        }
+
+        [MenuItem("Amazon GameLift/Host with Anywhere", priority = 101)]
+        public static void OpenAnywhereTab()
+        {
+            GetPluginWindow().OpenTab(GameLiftPlugin.Pages.Anywhere);
+        }
+
+        [MenuItem("Amazon GameLift/Host with Managed EC2", priority = 102)]
+        public static void OpenEC2Tab()
+        {
+            GetPluginWindow().OpenTab(GameLiftPlugin.Pages.ManagedEC2);
+        }
+
+        [MenuItem("Amazon GameLift/Import Sample Game", priority = 103)]
+        public static void ImportSampleGame()
+        {
+            AssetDatabase.ImportPackage(_filePackagePath, interactive: true);
+        }
+
+        [MenuItem("Amazon GameLift/Help/Documentation", priority = 200)]
+        public static void OpenDocumentation()
+        {
+            Application.OpenURL(Urls.AwsHelpGameLiftUnity);
+        }
+
+        [MenuItem("Amazon GameLift/Help/AWS GameTech Forum", priority = 201)]
+        public static void OpenGameTechForums()
+        {
+            Application.OpenURL(Urls.AwsGameTechForums);
+        }
+
+        [MenuItem("Amazon GameLift/Help/Report Issues", priority = 202)]
+        public static void OpenReportIssues()
+        {
+            Application.OpenURL(Urls.GitHubAwsLabs);
+        }
+    }
+}
