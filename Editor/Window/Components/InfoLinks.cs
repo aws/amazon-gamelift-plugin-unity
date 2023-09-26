7075bb8fdaf46791edf057db694bb0cfcbc5a475
Kyle-CW <109524503+Kyle-CW@users.noreply.github.com>
2023-09-26 02:29:34 +0100

    Feature/anywhere info links (#106)
    * info links wip
    
    * Added info links styling
    
    * renamed divider class
    
    * renamed component prefix to `gl`
    
    * removed empty class attributes
    
    * removed default style redeclaration
    
    * moved font colour declaration
    
    * Updated namespace
    
    * Updated namespace
    
    * Updated css variable name
    
    * Added infolinks to anywhere pages

---
+++ Editor/Window/Components/InfoLinks.cs
@@ -0,0 +1,45 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using AmazonGameLift.Editor;
+using Editor.Resources.EditorWindow;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AmazonGameLift.Editor
+{
+    public class InfoLinks : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<InfoLinks> { }
+        
+        public InfoLinks()
+        {
+            var asset = Resources.Load<VisualTreeAsset>("EditorWindow/Components/InfoLinks");
+            asset.CloneTree(this);
+
+            LocalizeText();
+
+            this.Q<Label>("InfoLinkDocumentationLink")
+                .RegisterCallback<ClickEvent>(_ => OnLinkClicked(Urls.AwsHelpGameLiftUnity));
+            this.Q<Label>("InfoLinkForumLink").RegisterCallback<ClickEvent>(_ => OnLinkClicked(Urls.AwsGameTechForums));
+            this.Q<Label>("InfoLinkTroubleshootingLink")
+                .RegisterCallback<ClickEvent>(_ => OnLinkClicked("")); // TODO: Get correct action
+            this.Q<Label>("InfoLinkReportIssuesLink")
+                .RegisterCallback<ClickEvent>(_ => OnLinkClicked(Urls.GitHubAwsLabs));
+        }
+
+        private void OnLinkClicked(string url)
+        {
+            Application.OpenURL(url);
+        }
+
+        private void LocalizeText()
+        {
+            var l = new ElementLocalizer(this);
+            l.SetElementText("InfoLinkDocumentationLink", Strings.InfoLinkDocumentationLink);
+            l.SetElementText("InfoLinkForumLink", Strings.InfoLinkForumLink);
+            l.SetElementText("InfoLinkTroubleshootingLink", Strings.InfoLinkTroubleshootingLink);
+            l.SetElementText("InfoLinkReportIssuesLink", Strings.InfoLinkReportIssuesLink);
+        }
+    }
+}
