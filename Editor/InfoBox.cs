9dc46c65f72639bc860920606eb5ea7e29446e0a
ChefCarlos <carlind95@gmail.com>
2023-09-26 13:56:33 +0100

    ASG6: Initial Error box implementation;

---
+++ Editor/InfoBox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace AmazonGameLift.Editor
+{
+    public abstract class InfoBox
+    {
+        public VisualElement BoxElement { get; set; }
+        public bool ShowElement { get; set; }
+        
+        public InfoBox(VisualElement boxElement, bool showElement)
+        {
+            BoxElement = boxElement;
+            ShowElement = showElement;
+        }
+    }
+}
---
+++ Editor/InfoBoxes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AmazonGameLift.Editor
+{
+    public class InfoBoxes
+    {
+        private readonly List<InfoBox> _allInfoBoxes = new();
+        private const string  InactiveInfoBoxElementName = "info-box__hidden";
+
+        public void AddInfoBoxElements(IEnumerable<InfoBox> infoBoxes)
+        {
+            _allInfoBoxes.AddRange(infoBoxes);
+        }
+
+        public void UpdateInfoBoxesState()
+        {
+            foreach (var infoBox in _allInfoBoxes)
+            {
+                if (infoBox.ShowElement)
+                {
+                    infoBox.BoxElement.AddToClassList(InactiveInfoBoxElementName);
+                }
+                else
+                {
+                    infoBox.BoxElement.RemoveFromClassList(InactiveInfoBoxElementName);
+                }
+            }
+        }
+    }
+}
