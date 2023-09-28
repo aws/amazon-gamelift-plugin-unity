65d3ec399fa0bee91376730ce0a0486451ad43cb
Kyle Young <kyle.young@codewizards.co.uk>
2023-09-28 14:00:10 +0100

    Removed unused property

--- Editor/CoreAPI/UserProfile.cs
+++ Editor/CoreAPI/UserProfile.cs
@@ -3,21 +3,19 @@
     public class UserProfile
     {
         public string Region { get; set; }
-        
+
         public string Name { get; set; }
-        
+
         public string BootStrappedBucket { get; set; }
-        
+
         public string FleetName { get; set; }
-        
+
         public string FleetId { get; set; }
 
         public string ComputeName { get; set; }
-        
-        public string IpAddress { get; set; }
-        
-        public string WebSocketUrl { get; set; }
 
-        public bool IsSelected { get; set; }
+        public string IpAddress { get; set; }
+
+        public string WebSocketUrl { get; set; }
     }
 }
