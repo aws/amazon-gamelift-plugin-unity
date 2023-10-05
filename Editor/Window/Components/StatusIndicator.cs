a6468237960a798c1beca8b05ecad17c556af41d
Kyle Young <kyle.young@codewizards.co.uk>
2023-10-05 14:39:36 +0100

    Added status indicator component

---
+++ Editor/Window/Components/StatusIndicator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AmazonGameLift.Editor
+{
+    public class StatusIndicator : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<StatusIndicator> { }
+
+        private Label _text => this.Q<Label>();
+
+        private readonly Dictionary<State, string> _stateClassNames = new()
+        {
+            { State.Inactive, "status-indicator--inactive" },
+            { State.InProgress, "status-indicator--in-progress" },
+            { State.Success, "status-indicator--success" },
+            { State.Failed, "status-indicator--failed" },
+        };
+
+        public StatusIndicator()
+        {
+            var uxml = Resources.Load<VisualTreeAsset>("EditorWindow/Components/StatusIndicator");
+            uxml.CloneTree(this);
+            AddToClassList("separator");
+            AddToClassList("separator--horizontal");
+        }
+
+        public void Set(State state, string text)
+        {
+            Reset();
+            _text.text = text;
+            AddToClassList(_stateClassNames[state]);
+        }
+
+        private void Reset()
+        {
+            foreach (var className in _stateClassNames.Values)
+            {
+                RemoveFromClassList(className);
+            }
+        }
+    }
+
+    public enum State
+    {
+        Inactive,
+        InProgress,
+        Success,
+        Failed
+    }
+}
