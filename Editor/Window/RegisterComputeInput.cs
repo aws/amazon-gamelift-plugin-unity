4881dfc67763ca22ccb6ae7f3bfcf0e44f60915e
ChefCarlos <carlind95@gmail.com>
2023-11-06 12:18:35 +0000

    Fixes to RegisterComputeInput; Will use correct compute name; make fields readonly if not changable.

--- Editor/Window/RegisterComputeInput.cs
+++ Editor/Window/RegisterComputeInput.cs
@@ -165,6 +165,8 @@ namespace AmazonGameLift.Editor
                 _computeState = ComputeStatus.Registered;
             }
 
+            _computeNameInput.value = _stateManager.ComputeName;
+
             UpdateGUI();
         }
 
@@ -220,6 +222,26 @@ namespace AmazonGameLift.Editor
             _registerComputeStatusBox = _container.Q<StatusBox>("AnywherePageComputeStatusBox");
         }
 
+        private void CheckReadOnly()
+        {
+            if (_computeState == ComputeStatus.Registered)
+            {
+                _computeNameInput.isReadOnly = true;
+                foreach (var ipBox in _ipInputs)
+                {
+                    ipBox.isReadOnly = true;
+                }
+            }
+            else
+            {
+                _computeNameInput.isReadOnly = false;
+                foreach (var ipBox in _ipInputs)
+                {
+                    ipBox.isReadOnly = false;
+                }
+            }
+        }
+
         protected sealed override void UpdateGUI()
         {
             var elements = GetVisibleItemsByState();
@@ -244,6 +266,8 @@ namespace AmazonGameLift.Editor
                 _statusIndicator.Set(State.Success,
                     textProvider.Get(Strings.AnywherePageComputeStatusRegistered));
             }
+
+            CheckReadOnly();
         }
 
         private void LocalizeText()
